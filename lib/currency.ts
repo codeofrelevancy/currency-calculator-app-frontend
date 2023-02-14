2c399f6a504551397bc292aee33f41ccb2c79bf5
codeofrelevancy <codeofrelevancy@gmail.com>
2023-02-14 13:38:25 +0530

    debug

--- lib/currency.ts
+++ lib/currency.ts
@@ -4,8 +4,12 @@ import { db } from '@/firebase';
 
 export const getConversionRates = async () => {
   try {
+    console.log('DB:', db);
+    
     const docRef = doc(db, 'currency', 'conversion_rates');
+    console.log('docRef:', docRef);
     const docSnap = await getDoc(docRef);
+    console.log('docSnap:', docSnap);
     
     return docSnap.data();
   } catch (error) {
